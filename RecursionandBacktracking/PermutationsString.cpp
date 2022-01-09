38eb9b81abf5d94642b526fc43d49e0d3ff144b7
agwlprince617 <agrawalprince617@gmail.com>
2022-01-09 10:37:26 +0530

    Added all possible solutions

--- RecursionandBacktracking/PermutationsString.cpp
+++ RecursionandBacktracking/PermutationsString.cpp
@@ -1,6 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
+
+
+//Using extra space 
+   void findPermute(vector<int> &nums,vector<int>&ds,vector<vector<int>> &ans,bool freq[]){
+        if(nums.size()==ds.size()){
+            ans.push_back(ds);
+            return;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(!freq[i]){
+                ds.push_back(nums[i]);
+                freq[i]=1;
+                findPermute(nums,ds,ans,freq);
+                freq[i]=0;
+                ds.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int>ds;
+        bool freq[nums.size()];
+        for(int i=0;i<nums.size();i++) freq[i]=0;
+        findPermute(nums,ds,ans,freq);
+        return ans;
+    }        
+
+    //Most optimised using less space
     void permute(int ind,vector<int> &nums,vector<vector<int>> &ans){
         if(ind==nums.size()){
             ans.push_back(nums);
@@ -18,3 +47,18 @@ using namespace std;
         permute(0,nums,ans);
         return ans;
     }
+
+
+
+
+//Using stl next permutation
+	vector<string>find_permutation(string S)
+		{
+		    // Code here there
+		    sort(S.begin(),S.end());
+		    vector<string> v;
+		    do{
+		        v.push_back(S);
+		    }while(next_permutation(S.begin(),S.end()));
+		    return v;
+		}    
