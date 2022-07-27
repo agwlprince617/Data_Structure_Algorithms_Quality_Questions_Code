92b8ab42f8181d02287f28f504af950c50415f1e
agwlprince617 <agrawalprince617@gmail.com>
2022-07-28 00:45:01 +0530

    new problems

---
+++ LinkedList/RemoveDuplicats.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+       ListNode* curr=head;
+        while(curr and curr->next){
+            if(curr->val==curr->next->val){
+                curr->next=curr->next->next;
+            }
+            else{
+                curr=curr->next;
+            }
+        }
+        return head;
+    }
+};
